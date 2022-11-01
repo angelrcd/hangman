c8037cd229738e1edb735cfc9a232296eb088526
Angel Romero <mrangelrcd@gmail.com>
2022-11-01 23:21:19 +0100

    added lose state

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 const livesBoxes = document.querySelectorAll(".lives");
 const letterBoxes = document.querySelectorAll(".letters");
 const letterButtons = document.querySelectorAll(".letter-button");
+const resetButton = document.querySelector("#reload");
+const endMessage = document.querySelector("#endMessage");
 const wordToGuess = "ALADOS";
+
 let lostLives = 0;
 
 function fillLifeBox() {
   livesBoxes[lostLives].classList.add("bg-red-600");
   lostLives++;
+}
+
+function reset() {
+  lostLives = 0;
+  endMessage.textContent = "";
+  livesBoxes.forEach(box => {
+    box.classList.remove("bg-red-600");
+  });
+  letterBoxes.forEach(box => {
+    box.textContent = "";
+  });
+  letterButtons.forEach(box => {
+    box.classList.remove("bg-cyan-600");
+    box.classList.add("hover:bg-cyan-50");
+  });
+  letterButtons.forEach(button => {
+    button.disabled = false;
+  });
 }
 
 function fillLetterButton(box) {
@@ -20,7 +41,6 @@ function getPositionsOfGuess(letter) {
   for (let i = 0; i < array.length; i++) {
     if (array[i] === letter) resultados.push(i);
   }
-  console.log(resultados);
   return resultados;
 }
 
@@ -33,9 +53,24 @@ function applyGuess(letter, guessPositions) {
   });
 }
 
+function checkLose() {
+  if (lostLives === 6) {
+    letterButtons.forEach(button => {
+      button.disabled = true;
+    });
+    endMessage.classList.add("text-red-500");
+    endMessage.textContent = "HAS PERDIDO";
+  }
+}
+
 letterButtons.forEach(box => {
   box.addEventListener("click", () => {
     fillLetterButton(box);
     applyGuess(box.textContent, getPositionsOfGuess(box.textContent));
+    checkLose();
   });
+});
+
+resetButton.addEventListener("click", () => {
+  reset();
 });
